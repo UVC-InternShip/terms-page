d1a6366aeb2033733730c6db7b6c9dc3a70308a7
khakisage <jongjin201@gmail.com>
2025-01-09 18:30:48 +0900

    라우터 수정: 약관 경로를 변경하고 개인정보 보호 페이지 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Terms } from "./pages/Terms";
 import { Contact } from "./pages/Contact";
+import { Terms } from "./pages/Terms";
+import { Privacy } from "./pages/TermsOfPrivacy";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/terms" element={<Terms />} />
+        <Route path="/terms/*" element={<Terms />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
