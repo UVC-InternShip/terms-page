cc1ab787eed0192095d92f4c52dec7239286f3c6
khakisage <jongjin201@gmail.com>
2025-01-10 14:50:20 +0900

    Vite 설정에서 기본 경로 주석 처리

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/terms-page/",
+  // base: "/terms-page/",
   plugins: [react()],
 });
